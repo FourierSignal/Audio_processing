5bf24bfaeb5fca818a39864f6e6b06fb149e7746
Sudheer <sudheer.linx@gmail.com>
2022-05-02 09:07:36 +0530

    Update ops_wavefile.py

--- ops_wavefile.py
+++ ops_wavefile.py
@@ -58,3 +58,15 @@ sf.write('new_file.wav', data, 44100)
 data, fs = sf.read('new_file.wav', dtype='float32')  
 sd.play(data, fs)
 status = sd.wait()
+
+#------------------------------------
+import soundfile as sf
+import sounddevice as sd
+from scipy.io.wavfile import write
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)  # Save as WAV file
+data, fs = sf.read('output.wav')
+sd.play(data*20,fs)
